8a8ddd1c2f1edc5ef481bba16ffc64e7a76e9b99
Timothy Ogbogu <timothyogbogu@Timothys-MacBook-Pro.local>
2025-09-19 02:16:51 +0100

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.bullvest.bullvest"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.bullvest.com"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 17
+        versionName = "1.0.1"
+    }
+
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("bullvest.jks")
+            storePassword = "bullvest101@"
+            keyAlias = "bullvest"
+            keyPassword = "bullvest101@"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/bullvest/bullvest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bullvest.bullvest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
